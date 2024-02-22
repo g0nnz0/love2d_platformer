aa531a1857db8abd8280b8efcd00e203f34698e5
g0nnz0 <bonzai_rock@hotmail.com>
2024-02-22 18:32:55 -0300

    update(main): improve in colission system, walls don't check collision with each other

--- main.lua
+++ main.lua
@@ -12,6 +12,10 @@ function love.load()
     table.insert(objects, player)
     table.insert(objects, box)
 
+    --para mejorar la eficiencia, colocaremos cada wall en una tabla walls, de lo contrario las walls
+    --estarian chequeando colisiones entre si lo cual no tiene sentido en este proyecto.
+    walls = {}
+
     map = {
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -33,8 +37,8 @@ function love.load()
         for j,w in ipairs(v) do
             --y chequeá si es igual a 1.
             if w==1 then
-                --si lo es insertá un objeto de tipo Wall en la tabla objects(las posiciones que marca creo que es para que cada wall esté centrada)
-                table.insert(objects, Wall((j-1)*50, (i-1)*50))
+                --si lo es insertá un objeto de tipo Wall en la tabla walls(las posiciones que marca creo que es para que cada wall esté centrada)
+                table.insert(walls, Wall((j-1)*50, (i-1)*50))
             end
         end
     end
@@ -44,7 +48,11 @@ end
 
 function love.update(dt)
    --mediante un loop llamamos update() en cada objeto de la table objects
-    for i,v in ipairs(objects) do 
+   for i,v in ipairs(objects) do 
+    v:update(dt)
+   end
+
+   for i,v in ipairs(walls) do
     v:update(dt)
    end
 
@@ -63,18 +71,31 @@ function love.update(dt)
             break
         end
 
-   --Mediante un loop anidado resolvemos las colisiones
-   --en este primer loop recorremos todos los objetos menos el ultimo
+        --Mediante un loop anidado resolvemos las colisiones
+        --en este primer loop recorremos todos los objetos menos el ultimo
         for i=1, #objects-1 do
-        --en este loop recorremos los objetos empezando por el que le sigue al primer objeto, y esta vez incluimos al ultimo
-                for j=i+1, #objects do
-        --de esta manera no estamos llamando resoveCollision() 2 veces por objeto.    
-                    local collision = objects[i]:resolveCollision(objects[j])
-                   if collision then
+            --en este loop recorremos los objetos empezando por el que le sigue al primer objeto, y esta vez incluimos al ultimo
+            for j=i+1, #objects do
+                --de esta manera no estamos llamando resoveCollision() 2 veces por objeto.    
+                local collision = objects[i]:resolveCollision(objects[j])
+                if collision then
                         loop = true
-                   end
                 end
+            end
         end
+
+        --mediante otro loop anidado resuelvo las colisiones con las wall
+        --por cada wall en walls...
+        for i,wall in ipairs(walls) do
+            --iterá en cada objeto de objects...
+            for i, object in ipairs(objects) do
+                --chequea si hay colision con la wall en donde se encuentre...
+                local colission = object:resolveCollision(wall)
+                if collision then
+                    loop = true
+                end
+            end
+        end 
     end
 
 end
@@ -82,8 +103,13 @@ end
 
 
 function love.draw()
-    --con otro loop dibujamos todos los objetos
+    --con otro loop dibujamos el player y la box
     for i,v in ipairs(objects) do
+        v:draw()
+    end
+
+    --con otro loop dibujamos las paredes
+    for i,v in ipairs(walls) do
         v:draw()
     end
    
